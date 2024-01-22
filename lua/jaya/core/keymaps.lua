6fd71eed5f1997ed0890662d837f7369e423b949
jayadeep11 <jaydeep.bellamkonda@gmail.com>
2024-01-22 18:36:48 +0530

    edited keymaps

--- lua/jaya/core/keymaps.lua
+++ lua/jaya/core/keymaps.lua
@@ -26,6 +26,10 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- Git
+vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit -m \"", {noremap=false})
+vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push -u origin HEAD<CR>", {noremap=false})
 
-
+-- twilight
+vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", {noremap=false})
 
